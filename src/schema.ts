a7b266c65ce9edecaa53ed97dedc6c528d23eac9
Antonio <antonio@iovlabs.org>
2025-06-25 15:05:50 +0200

    chore: apply small changes

--- src/schema.ts
+++ src/schema.ts
@@ -2,6 +2,7 @@ import { Bytes, ethereum } from "@graphprotocol/graph-ts";
 import { Backer as BackerEntity } from "../generated/schema";
 import { updateBlockInfo } from "./utils";
 
+// TODO: not used for now, but we're going to implement this idea in the future
 export class Backer extends BackerEntity { 
 
     constructor(id: Bytes) {
