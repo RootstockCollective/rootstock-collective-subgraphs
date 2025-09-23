3148cb86ea5a3e841c63fc300581952aa064ac2a
franciscotobar <100875069+franciscotobar@users.noreply.github.com>
2025-09-23 11:14:58 -0600

    TOK-776/notify-reward (#7)
    * refactor: get cycle duration
    
    * refactor: pr comments

--- src/backersManager/notifyReward.ts
+++ src/backersManager/notifyReward.ts
@@ -6,17 +6,23 @@ import { Bytes } from "@graphprotocol/graph-ts";
 export function handleNotifyReward(
   event: NotifyRewardEvent
 ): void {
-  const backersManagerContract = BackersManagerRootstockCollectiveContract.bind(
-    event.address
-  );
-  const cycleStart = backersManagerContract.cycleNext(event.block.timestamp);
-  const cycle = loadOrCreateCycle(changetype<Bytes>(Bytes.fromBigInt(cycleStart)));
-  if(event.params.rewardToken_.equals(backersManagerContract.rewardToken())) { 
+  const backersManagerContract = BackersManagerRootstockCollectiveContract.bind(event.address);
+  const nextCycleStart = backersManagerContract.cycleNext(event.block.timestamp);
+  const currentCycleStart = backersManagerContract.cycleStart(event.block.timestamp);
+  const cycle = loadOrCreateCycle(changetype<Bytes>(Bytes.fromBigInt(nextCycleStart)));
+
+  cycle.previousCycleStart = currentCycleStart;
+  cycle.previousCycleDuration = backersManagerContract.getCycleStartAndDuration().getValue1();
+  cycle.currentCycleStart = nextCycleStart;
+  cycle.distributionDuration = backersManagerContract.distributionDuration();
+
+  // Update rewards for this cycle
+  if (event.params.rewardToken_.equals(backersManagerContract.rewardToken())) {
     cycle.rewardsERC20 = cycle.rewardsERC20.plus(event.params.amount_);
   } else {
     cycle.rewardsRBTC = cycle.rewardsRBTC.plus(event.params.amount_);
   }
-  
+
   cycle.save();
 
   updateBlockInfo(event, ["Cycle"]);
--- src/backersManager/rewardDistributionStarted.ts
+++ src/backersManager/rewardDistributionStarted.ts
@@ -1,6 +1,6 @@
 import { RewardDistributionStarted as RewardDistributionStartedEvent } from "../../generated/BackersManagerRootstockCollective/BackersManagerRootstockCollective";
 import { BackersManagerRootstockCollective as BackersManagerRootstockCollectiveContract } from "../../generated/BackersManagerRootstockCollective/BackersManagerRootstockCollective";
-import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { Bytes } from "@graphprotocol/graph-ts";
 import { loadOrCreateCycle, updateBlockInfo } from "../utils";
 
 export function handleRewardDistributionStarted(
@@ -9,24 +9,12 @@ export function handleRewardDistributionStarted(
   const backersManagerContract = BackersManagerRootstockCollectiveContract.bind(
     event.address
   );
+
+  backersManagerContract.cycleData();
   const currentCycleStart = backersManagerContract.cycleStart(event.block.timestamp);
-  const cycleData = backersManagerContract.cycleData();
 
-  // Duration of the finalized cycle (N)
-  let finalizedDuration: BigInt;
-  if (currentCycleStart.equals(cycleData.getNextStart())) {
-    // Just switched into the first new cycle → previous cycle used previousDuration
-    finalizedDuration = cycleData.getPreviousDuration();
-  } else {
-    // Already past the first new cycle → previous cycle used nextDuration
-    finalizedDuration = cycleData.getNextDuration();
-  }
-
-  const finalizedCycleStart = currentCycleStart.minus(finalizedDuration);
   const cycle = loadOrCreateCycle(changetype<Bytes>(Bytes.fromBigInt(currentCycleStart)));
   cycle.onDistributionPeriod = true;
-  cycle.previousCycleStart = finalizedCycleStart;
-  cycle.previousCycleDuration = finalizedDuration;
   cycle.currentCycleStart = currentCycleStart;
   cycle.currentCycleDuration = backersManagerContract.getCycleStartAndDuration().getValue1();
   cycle.distributionDuration = backersManagerContract.distributionDuration();
